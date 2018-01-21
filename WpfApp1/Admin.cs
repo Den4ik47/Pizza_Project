4dc0c1cf463f0199d854d9dafa72a7f8b3ec7a6b
Denys <119844@student.uni.opole.pl>
2018-01-21 20:46:57 +0100

    Second

--- WpfApp1/Admin.cs
+++ WpfApp1/Admin.cs
@@ -8,6 +8,7 @@ namespace WpfApp1
 {
     public class Admin
     {
+        public int d=0;
         public string log = "Administration";
         public string password = "123";
         public Admin() { }
